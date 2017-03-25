c33bba23e7cd7f8a2772724b4e860b80a34aa4cf
griffrick <rickleg93@gmail.com>
2017-03-25 14:11:19 -0400

    Removed erroneous migrations

--- db/migrate/20170308003226_create_placelists.rb
+++
@@ -1,8 +0,0 @@
-class CreatePlacelists < ActiveRecord::Migration[5.0]
-  def change
-    create_table :placelists do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20170308003233_create_places.rb
+++
@@ -1,8 +0,0 @@
-class CreatePlaces < ActiveRecord::Migration[5.0]
-  def change
-    create_table :places do |t|
-
-      t.timestamps
-    end
-  end
-end
